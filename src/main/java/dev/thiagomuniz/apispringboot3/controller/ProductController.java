86308b983f4a5d17e0ae3db2c1168a7ec5a6b282
Thiago Muniz <muniz@duck.com>
2023-06-23 03:22:05 -0400

    implemented hateoas

--- src/main/java/dev/thiagomuniz/apispringboot3/controller/ProductController.java
+++ src/main/java/dev/thiagomuniz/apispringboot3/controller/ProductController.java
@@ -10,11 +10,11 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
-import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
-
 import java.util.List;
 import java.util.UUID;
+
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
 
 @RestController
 public class ProductController {
--- src/main/java/dev/thiagomuniz/apispringboot3/repository/ProductRepository.java
+++ src/main/java/dev/thiagomuniz/apispringboot3/repository/ProductRepository.java
@@ -1,6 +1,5 @@
 package dev.thiagomuniz.apispringboot3.repository;
 
-import dev.thiagomuniz.apispringboot3.dto.ProductRecordDto;
 import dev.thiagomuniz.apispringboot3.model.ProductModel;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
