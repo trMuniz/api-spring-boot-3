6e63707fa4672d0cddc09a13768425b3fa1f029b
Thiago Muniz <muniz@duck.com>
2023-06-23 01:50:20 -0400

    initial commit

---
+++ src/main/java/dev/thiagomuniz/apispringboot3/ApiSpringBoot3Application.java
@@ -0,0 +1,13 @@
+package dev.thiagomuniz.apispringboot3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiSpringBoot3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiSpringBoot3Application.class, args);
+	}
+
+}
---
+++ src/test/java/dev/thiagomuniz/apispringboot3/ApiSpringBoot3ApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.thiagomuniz.apispringboot3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiSpringBoot3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
